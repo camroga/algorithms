2a14e50bee6633e389623b362791114858ed1a6d
Camilo Andrés Rodríguez Garzón <cam_rod@hotmail.com>
2025-01-18 22:18:10 +0400

    solve binary-tree-level-order-traversal problem

--- src/main/kotlin/binary_tree/binary_tree_level_order_traversal.kt
+++ src/main/kotlin/binary_tree/binary_tree_level_order_traversal.kt
@@ -1,8 +1,7 @@
-import binary_tree.BinaryTree
-import java.util.ArrayDeque;
+import java.util.*
 
 
-fun levelOrder(root: TreeNode?): List<List<Int>> {
+fun levelOrderOld(root: TreeNode?): List<List<Int>> {
     if (root == null) return listOf<List<Int>>()
     // bfs
     var queue = mutableListOf<TreeNode>(root)
@@ -22,8 +21,31 @@ fun levelOrder(root: TreeNode?): List<List<Int>> {
     return lo
 }
 
+fun levelOrder(root: TreeNode?): List<List<Int>> {
+    if (root == null) return emptyList()
+    val result = mutableListOf<List<Int>>()
+    val queue = ArrayDeque<TreeNode>().apply { add(root) }
 
-fun main(args: Array<String>) {
+    while (queue.isNotEmpty()) {
+        val levelSize = queue.size
+        val level = mutableListOf<Int>()
+
+        repeat(levelSize) {
+            val node = queue.removeFirst()
+            level.add(node.`val`)
+
+            node.left?.let { queue.add(it) }
+            node.right?.let { queue.add(it) }
+        }
+
+        result.add(level)
+    }
+
+    return result
+}
+
+
+fun main() {
     val root = TreeNode(1).apply {
         right = TreeNode(3).apply {
             right = TreeNode(5)
